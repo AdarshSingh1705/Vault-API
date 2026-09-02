c068792c33e91a3284532e8bf6bd8e9578542c7f
AdarshSingh1705 <as.adarshsingh13@gmail.com>
2026-09-03 04:12:49 +0530

    repository has been updated

---
+++ src/main/java/com/adarsh/vault_api/Repository/AccountRepository.java
@@ -0,0 +1,14 @@
+package com.adarsh.vault_api.Repository;
+
+import com.adarsh.vault_api.Entity.BankAccount;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface AccountRepository extends JpaRepository<BankAccount, Long> {
+    boolean existsByAccountNumber(String accountNumber);
+
+    Optional<BankAccount> findByAccountNumber(String accountNumber);
+    List<BankAccount> findByOwner_Email (String email);
+}
